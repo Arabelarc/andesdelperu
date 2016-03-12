200e50a40b883e5badfbe408a4a762360f03a74d
arabelarc <arabela@laboratoria.la>
2016-03-12 13:12:29 -0500

    vistas agregadas

--- src/wordpress_theme_bp/header.php
+++ src/wordpress_theme_bp/header.php
@@ -1,11 +1,86 @@
 <!doctype html>
 <html lang="en">
-	<head>
-		<meta charset="utf-8">
-		<meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=no">
-		<link rel="shortcut icon" href="<?php echo get_template_directory_uri(); ?>/app/assets/img/favicon.png">
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+    <meta name="description" content="">
+    <meta name="keywords" content=""> 
+    <meta name="author" content="@ArabelaRojas">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=no">
+    <link rel="shortcut icon" href="assets/img/favicon.png">
+    <script src="//use.typekit.net/fct8sgn.js"></script>
+    <script>try{Typekit.load();}catch(e){}</script>
+    <script type="text/javascript" src="http://code.jquery.com/jquery-2.1.4.min.js"></script>
+    <link href="<?php echo get_template_directory_uri(); ?>/app/assets/css/style.css" rel="stylesheet"> <!-- App styles -->
+    <link href="<?php echo get_template_directory_uri(); ?>/app/assets/css/ebm.css" rel="stylesheet">
+        <!-- Owl Carousel Assets -->
+    <link href="<?php echo get_template_directory_uri(); ?>/app/assets/css/owl.carousel.css" rel="stylesheet">
+    <link href="<?php echo get_template_directory_uri(); ?>/app/assets/css/owl.theme.css" rel="stylesheet">
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=no">
+	<link rel="shortcut icon" href="<?php echo get_template_directory_uri(); ?>/app/assets/img/favicon.png">
 		<?php wp_head(); ?>
 	</head>
 	<body>
-		<div class="site-wrapper">			
+    <div class="site-wrapper"> <!-- Body -->
+      <header class="menu-section-mobile hidden-md hidden-lg bg-gray-dark minh-63 fixed">
+        <div class="logo-menu-mobile">
+          <a href=""><img src="assets/img/logo.png" alt="Logo"></a>
+        </div>
+        <div class="menu-toggle">
+          <div class="one"></div>
+          <div class="two"></div>
+          <div class="three"></div>
+        </div>
+        <nav>
+          <ul role="navigation" class="hidden text-uppercase ">
+            <li><a class="light font-s-20 pt-70" href="index.html">Inicio</a></li>
+            <hr width="50px">
+            <li><a class="light font-s-20 pv-7" href="index.html#site-content">Nosotros</a></li>
+            <hr width="50px">
+            <li><a class="light font-s-20 pv-7" href="galeria.html">Galeria</a></li>
+            <hr width="50px">
+            <li><a class="light font-s-20 pv-7" href="contacto">contacto</a></li>
+            <hr width="50px">
+          </ul>
+        </nav>
+      </header>
+      <header class="header-scroll header hidden-xs hidden-sm">
+        <div class="container">
+          <div class="row">
+            <div class="header-left col-sm-3 text-center">
+              <div class="logo"><a href=""><img src="assets/img/logo.png" class="img-responsive" alt="Logo"></a></div>
+            </div>
+              <div class="header-right col-sm-2"> 
+              </div>
+            <div class="header-right col-sm-7">
+              <nav class="header-nav-center pt-7 thin text-gray text-uppercase nav-list">
+                <a href="index.html">Inicio</a>
+                <a href="index.html#site-content">Nosotros</a>
+                <a href="galeria.html">galeria</a>
+                <a href="contacto.html">contacto</a>
+              </nav>
+            </div>
+          </div>
+        </div>
+      </header>
+      <header class="header hidden-xs hidden-sm" >
+        <div class="container">
+          <div class="row">
+            <div class="header-left col-sm-3 text-center">
+              <div class="logo"><a href=""><img src="assets/img/logo.png" class="img-responsive" alt="Logo Utec ventures"></a></div>
+            </div>
+              <div class="header-right col-sm-2"> 
+              </div>
+            <div class="header-right col-sm-7">
+              <nav class="header-nav-center pt-7 thin text-gray text-uppercase nav-list">
+                <a href="index.html">Inicio</a>
+                <a href="index.html#site-content">Nosotros</a>
+                <a href="galeria.html">galeria</a>
+                <a href="contacto.html">contacto</a>
+              </nav>
+            </div>
+          </div>
+        </div>
+      </header>		
 		
